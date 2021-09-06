7b49dab736f1816b8c3f8356364fe42fad7eacbf
Alejandro Guillen <aguillenv93@gmail.com>
2021-09-06 16:59:02 +0200

    Update todos http handler

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,15 +4,23 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getTodosForUser as getTodosForUser } from '../../helpers/todos'
 import { getUserId } from '../utils';
 
-// TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const userId = getUserId(event);
+    if (!userId) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify('User not found')
+      }
+    } 
     const todos = await getTodosForUser(userId);
-    return todos
+    return {
+      statusCode: 200,
+      body: JSON.stringify(todos)
+    }
   })
 
 handler.use(
